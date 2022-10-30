99e8155d308b35dd824396e8cb308375cda84574
YuryShageev <schageev.yur@yandex.ru>
2022-10-30 13:33:28 +0300

    Переделываю Классы и Объекты. Оказывается, забыл поставить геттер в Book для автора - из-за этого пошли проблемы. Добавил объект Author в Book. А потом его добавил в main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Classes and Objects");
 
+
+        System.out.println("Task 1");
+
+        Author johnScalzi = new Author("John", "Scalzi");
+        Book firstBook = new Book("Old Man's War", johnScalzi, 2005);
+        System.out.println(firstBook.getBookName() + firstBook.getPublishingYear());
     }
 }
