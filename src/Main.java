be07ed3672c620724168a717decaa548eaa4d4f3
YuryShageev <schageev.yur@yandex.ru>
2022-10-30 14:01:06 +0300

    Почему автора не видит?

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         Author johnScalzi = new Author("John", "Scalzi");
         Book firstBook = new Book("Old Man's War", johnScalzi, 2005);
-        System.out.println("Название книги = " + firstBook.getBookName() + " Автор = " + firstBook.getAuthor() + " Год выпуска = " + firstBook.getPublishingYear());
+        System.out.println("Название книги = " + firstBook.getBookName() + " Автор = " + johnScalzi + " Год выпуска = " + firstBook.getPublishingYear());
 
         firstBook.setPublishingYear(2006);
         System.out.println(firstBook.getPublishingYear());
