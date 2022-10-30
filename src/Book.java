ca5c19f68ec69b9b405f148b7a9268234c783d67
YuryShageev <schageev.yur@yandex.ru>
2022-10-30 11:12:57 +0300

    Переделываю Классы и Объекты

--- src/Book.java
+++ src/Book.java
@@ -1,12 +1,20 @@
 public class Book {
     private final String bookName;
-    private final Author author;
+    private Author author;
     private int publishingYear;
 
     public Book(String bookName, Author author, int publishingYear) {
         this.bookName = bookName;
         this.author = author;
         this.publishingYear = publishingYear;
+    }
+
+    public String getBookName() {
+        return this.bookName;
+    }
+
+    public int getPublishingYear() {
+        return this.publishingYear;
     }
 
     public void setPublishingYear(int publishingYear) {
