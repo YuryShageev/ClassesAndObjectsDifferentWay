4732137c57e84be3a54b5aee037e94957c1daa0f
YuryShageev <schageev.yur@yandex.ru>
2022-10-31 23:57:23 +0300

    Забыл убрать сеттер. Когда убрал, всё заработало))) Ура!! Вернул всё на место. Просто, надо было убедиться, что всё работает))

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ public class Main {
 
         System.out.println("Task 1");
 
-//        Author author = new Author("John", "Scalzi");
+//
         Book firstBook = new Book("Old Man's War", new Author("John", "Scalzi"), 2005);
         System.out.println("Название книги = " + firstBook.getBookName() + " Автор = " + firstBook.getAuthor() + " Год выпуска = " + firstBook.getPublishingYear());
-//        firstBook.setPublishingYear(2006);
-//        System.out.println(firstBook.getPublishingYear());
+        firstBook.setPublishingYear(2006);
+        System.out.println(firstBook.getPublishingYear());
 
         Book secondBook = new Book("The Boys From Brazil", new Author("Ira", "Levin"), 1970);
         System.out.println("Название книги = " + secondBook.getBookName() + " Автор = " + secondBook.getAuthor() + " Год выпуска = " + secondBook.getPublishingYear());
@@ -17,8 +17,9 @@ public class Main {
         Book thirdBook = new Book("Old Man's War", new Author("John", "Scalzi"), 2005);
         System.out.println("Название книги = " + thirdBook.getBookName() + " Автор = " + thirdBook.getAuthor() + " Год выпуска = " + thirdBook.getPublishingYear());
 
-//        Author author3 = new Author("John", "Scalzi");
-//        System.out.println(author3.equals(author));
+        Author author = new Author("John", "Scalzi");
+        Author author3 = new Author("John", "Scalzi");
+        System.out.println(author3.equals(author));
 
         System.out.println(thirdBook.equals(firstBook));
     }
