a11ccff57283da325f31ee073cbe4a6ee8810d4d
YuryShageev <schageev.yur@yandex.ru>
2022-11-01 23:54:59 +0300

    Геттеры из автора совсем убрал

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
         Book secondBook = new Book("The Boys From Brazil", new Author("Ira", "Levin"), 1970);
         System.out.println("Название книги = " + secondBook.getBookName() + " Автор = " + secondBook.getAuthor() + " Год выпуска = " + secondBook.getPublishingYear());
 
-        Book thirdBook = new Book("Old Man's War", new Author("John", "Scalzi"), 2005);
+        Book thirdBook = new Book("The Ghost Brigades", new Author("John", "Scalzi"), 2006);
         System.out.println("Название книги = " + thirdBook.getBookName() + " Автор = " + thirdBook.getAuthor() + " Год выпуска = " + thirdBook.getPublishingYear());
 
         Author author = new Author("John", "Scalzi");
